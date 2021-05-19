70bd262d64aaa933b67ef4be0b36c58d4bbfe95f
Astrid Lancrey <astrid.lancrey@gmail.com>
2021-05-19 22:34:40 +0200

    pouet

--- demo.py
+++ demo.py
@@ -447,7 +447,61 @@ def page_demo(state):
     #news_vectorizer = open("./Classificateur_slider_LTSM_0515_V2.pkl","rb")
     #news_cv = joblib.load(news_vectorizer)
     loaded_model = keras.models.load_model("./Classificateur_GRU256_ToyCar_5N.joblib")
-
+    
+    #CODE PREDICTION STREAMLIT
+    dt = 256
+    freq = 256
+    T_max=10
+    machine='ToyCar' 
+    path_model="/content/drive/MyDrive/Datascience projet son/Corentin/"
+    file_path="/content/drive/MyDrive/Datascience projet son/fichiers_audio/ToyCar/anomaly_id_01_00000011.wav"
+    file_name="anomaly_id_01_00000011.wav"
+    #constantes
+    seuil_machine={'ToyCar':[0.5,0.5,0.5,0.95,0.1],'ToyConveyor':[0.5,0.5,0.95,0.07],'fan':[0.5,0.5,0.5,0.95,0.1],'pump':[0.5,0.5,0.5,0.95,0.07],'slider':[0.5,0.95,0.5,0.5,0.05],'valve':[0.5,0.5,0.5,0.95,0.17]}
+    ID_machine={'ToyCar':["ID_2","ID_3","ID_4","ID_1"],'ToyConveyor':["ID_2","ID_3","ID_1"],'fan':["ID_4","ID_6","ID_0","ID_2"],'pump':["ID_0","ID_2","ID_4","ID_6"],'slider':["ID_0","ID_2","ID_4","ID_6"],'valve':["ID_0","ID_2","ID_4","ID_6"]}
+    print('Récupération des données audio')
+    data, fe = librosa.load(file_path, sr=None)
+    # For the audio > T_max : Use just the fist T_max seconde, to have the right shape. 
+    if len(data)>= T_max*fe:
+        data = data[:int(T_max*fe)]
+    # For the audio < T_max : Add in the signal a zeros vector, to have the right shape.
+    else :
+        data = np.concatenate([data, np.zeros(int(T_max*fe - len(data)))])
+    # Apply the logMelSpectrogram function. 
+    audio_to_pred = logMelSpectrogram(data[:(len(data)//dt)*dt], fe, np.around(T_max/dt,3))
+    audio_to_pred=audio_to_pred[:dt,:] # on tronque quand même au cas où nous avons dépasser le nombre de dt. 
+    audio_to_pred=(np.array(audio_to_pred)-np.array(audio_to_pred).min())/(np.array(audio_to_pred).max()-np.array(audio_to_pred).min())
+    # on cut pour avoir le bon nombre de fréquences
+    selector=np.where(audio_to_pred.max(axis=0)>np.sort(audio_to_pred.max(axis=0))[-(freq+1)],True,False) # on est très restrictif dans un premier temps 
+    if np.sum(selector)<freq:
+        selector=np.where(audio_to_pred.max(axis=0)>=np.sort(audio_to_pred.max(axis=0))[-(freq+1)],True,False) # si nous avons été trop restrictif, alors nous élargissons la selection puis nous bouclons pour supprimer le surplus
+    iter=100
+    while np.sum(selector)>freq and iter>0: # En cas de multiples valeurs similaires on boucle pour les supprimer et ne garder que la dim voulue
+        print('Trop de fréquences choisies, suppression de {:} fréquences pour atteindre la taille cible'.format( selector.sum()-freq))
+        selector[np.argsort(np.sort(audio_to_pred.max(axis=0)[selector]))[-1]]=False  # on récupère la position du plus petit élément de datamax pour la supprimer du sélector
+        iter-=1 # on met 10 itérations pour éviter de boucler à l'infini en cas d'erreur
+    audio_to_pred=audio_to_pred[:,selector].reshape(1,dt,freq)
+    print('Chargement du modèle')
+    Classificateur_GRU256=keras.models.load_model(path_model+"Classificateur_GRU256_"+machine+"_5N.joblib")
+    print('Prédictions')
+    y_pred = Classificateur_GRU256.predict(audio_to_pred)
+    y_pred_save = tuple(y_pred)
+    y_pred=y_pred.reshape(y_pred.shape[1])
+    # on réalise la prédiction d'ID
+    pred_ID=ID_machine[machine][y_pred[:y_pred.shape[0]-1].argmax()]
+    # on réalise la prédiction normal / anormal
+    if (seuil_machine[machine][y_pred[:y_pred.shape[0]-1].argmax()] > y_pred[y_pred[:y_pred.shape[0]-1].argmax()]) | (seuil_machine[machine][y_pred.shape[0]-1] < y_pred[y_pred.shape[0]-1]):
+    pred_anorm='anomaly'
+    else :
+    pred_anorm='normal'
+    true=df[(df.Machine== machine) & (df.fichier==file_name)]
+    cross_ID= 'bonne ' if pred_ID=='ID_'+str(true.iloc[0,5]) else 'fausse'
+    cross_norm= 'bonne ' if pred_anorm==true.iloc[0,6] else 'fausse'
+    print('Données réelles : ')  
+    print(machine+' ID_'+str(true.iloc[0,5])+" "+true.iloc[0,6]+ '\n')
+    print('Prédiction')
+    print("la machine ", machine, " ", pred_ID, " à un son ", pred_anorm)
+    print("la prédiction d'ID est "+cross_ID+", la prédiction de normalité est ",cross_norm)
     #Model_RNN = keras.models.load_model("Models/Classificateur_GRU256_ToyCar_5N.joblib")
     #st.image(image, width=None)
     # 2. AE ##################################################################
